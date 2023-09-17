4c9276c74901ef9c78985fdde0f044e113fa2fd3
AliOding12 <aliz17043@gmail.com>
2023-09-17 09:00:00 +0500

    Add encryption helper functions in encryption.php

--- includes/encryption.php
+++ includes/encryption.php
@@ -1,5 +1,5 @@
 <?php
 function decryptFile($encryptedData, $key, $iv) {
     return openssl_decrypt($encryptedData, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
-}
-?>
+}//can change the cipher_algo to what ever method 
+?>// Add encryption helper functions in encryption.php
